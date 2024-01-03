6538bb0e3639a135631aae6eb6c068fc4b34cbfb
Takahito Nakano <kano@ponko2.jp>
2024-01-04 00:56:21 +0900

    Chapter 2.3.5

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rand::{thread_rng, Rng};
+use std::{rc::Rc, sync::Mutex};
 use wasm_bindgen::{closure::Closure, JsCast, JsValue};
 
 pub fn main() -> Result<(), JsValue> {
@@ -18,20 +19,38 @@ pub fn main() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    let image = web_sys::HtmlImageElement::new().unwrap();
-    let callback = Closure::once(|| {
-        web_sys::console::log_1(&JsValue::from_str("loaded"));
+    wasm_bindgen_futures::spawn_local(async move {
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = Rc::clone(&success_tx);
+        let image = web_sys::HtmlImageElement::new().unwrap();
+
+        let callback = Closure::once(move || {
+            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                success_tx.send(Ok(()));
+            }
+        });
+
+        let error_callback = Closure::once(move |err| {
+            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                error_tx.send(Err(err));
+            }
+        });
+
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
+        image.set_src("/static/Idle (1).png");
+        success_rx.await;
+
+        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
+
+        sierpinski(
+            &context,
+            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
+            (0, 255, 0),
+            5,
+        );
     });
-    image.set_onload(Some(callback.as_ref().unchecked_ref()));
-    callback.forget();
-    image.set_src("/static/Idle (1).png");
-    context.draw_image_with_html_image_element(&image, 0.0, 0.0);
-    sierpinski(
-        &context,
-        [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-        (0, 255, 0),
-        5,
-    );
 
     Ok(())
 }
