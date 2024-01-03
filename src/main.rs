f6bdd7a5a2abaff068db4b6562c97865c20415ed
Takahito Nakano <kano@ponko2.jp>
2024-01-03 23:04:44 +0900

    Chapter 1.4.2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
-use wasm_bindgen::JsValue;
-use web_sys::console;
+use wasm_bindgen::{JsCast, JsValue};
 
 pub fn main() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
-    console::log_1(&JsValue::from_str("Hello, world!"));
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+    context.move_to(300.0, 0.0);
+    context.begin_path();
+    context.line_to(0.0, 600.0);
+    context.line_to(600.0, 600.0);
+    context.line_to(300.0, 0.0);
+    context.close_path();
+    context.stroke();
+    context.fill();
 
     Ok(())
 }
