4176c1c42483cfac6f06dc6a4b802ad6a2115c8e
Takahito Nakano <kano@ponko2.jp>
2024-01-04 22:45:11 +0900

    Chapter 3.2.3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+mod browser;
+
 use gloo_utils::format::JsValueSerdeExt;
 use serde::Deserialize;
 use std::{collections::HashMap, rc::Rc, sync::Mutex};
@@ -24,25 +27,12 @@ struct Sheet {
 pub fn main() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
-    let window = web_sys::window().unwrap();
-    let document = window.document().unwrap();
-    let canvas = document
-        .get_element_by_id("canvas")
-        .unwrap()
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .unwrap();
-    let context = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap();
+    let context = browser::context().expect("Could not get browser context");
 
-    wasm_bindgen_futures::spawn_local(async move {
-        let json = fetch_json("/static/rhb.json")
+    browser::spawn_local(async move {
+        let sheet: Sheet = browser::fetch_json("/static/rhb.json")
             .await
-            .expect("Could not fetch rhb.json");
-        let sheet: Sheet = json
+            .expect("Could not fetch rhb.json")
             .into_serde()
             .expect("Could not convert rhb.json into a Sheet structure");
         let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
@@ -84,21 +74,16 @@ pub fn main() -> Result<(), JsValue> {
                 stripe.frame.h.into(),
             );
         }) as Box<dyn FnMut()>);
-        window.set_interval_with_callback_and_timeout_and_arguments_0(
-            interval_callback.as_ref().unchecked_ref(),
-            50,
-        );
+        browser::window()
+            .unwrap()
+            .set_interval_with_callback_and_timeout_and_arguments_0(
+                interval_callback.as_ref().unchecked_ref(),
+                50,
+            );
         interval_callback.forget();
 
         success_rx.await;
     });
 
     Ok(())
-}
-
-async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
-    let window = web_sys::window().unwrap();
-    let resp_value = wasm_bindgen_futures::JsFuture::from(window.fetch_with_str(json_path)).await?;
-    let resp: web_sys::Response = resp_value.dyn_into()?;
-    wasm_bindgen_futures::JsFuture::from(resp.json()?).await
 }
