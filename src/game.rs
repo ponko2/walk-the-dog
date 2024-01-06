fd92d4004c6e7a36e7a4c6970938896a949b8e85
Takahito Nakano <kano@ponko2.jp>
2024-01-07 03:53:19 +0900

    Chapter 5.6.2

--- src/game.rs
+++ src/game.rs
@@ -98,6 +98,14 @@ impl RedHatBoy {
         self.state_machine = self.state_machine.update();
     }
 
+    fn pos_y(&self) -> i16 {
+        self.state_machine.context().position.y
+    }
+
+    fn velocity_y(&self) -> i16 {
+        self.state_machine.context().velocity.y
+    }
+
     fn frame_name(&self) -> String {
         format!(
             "{} ({}).png",
@@ -511,7 +519,7 @@ mod red_hat_boy_states {
     pub struct RedHatBoyContext {
         pub frame: u8,
         pub position: Point,
-        velocity: Point,
+        pub velocity: Point,
     }
 
     impl RedHatBoyContext {
@@ -623,7 +631,11 @@ impl Game for WalkTheDog {
                 .bounding_box()
                 .intersects(&walk.platform.bounding_box())
             {
-                walk.boy.land_on(walk.platform.bounding_box().y);
+                if walk.boy.velocity_y() > 0 && walk.boy.pos_y() < walk.platform.position.y {
+                    walk.boy.land_on(walk.platform.bounding_box().y);
+                } else {
+                    walk.boy.knock_out();
+                }
             }
             if walk
                 .boy
