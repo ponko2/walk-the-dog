6f730c778d33409a6c4100b17f178840798c0218
Takahito Nakano <kano@ponko2.jp>
2024-01-08 07:54:03 +0900

    Chapter 8.4.0.5

--- src/browser.rs
+++ src/browser.rs
@@ -143,6 +143,11 @@ pub fn hide_ui() -> Result<()> {
         ui.remove_child(&child)
             .map(|_removed_child| ())
             .map_err(|err| anyhow!("Failed to remove child {:#?}", err))
+            .and_then(|_unit| {
+                canvas()?
+                    .focus()
+                    .map_err(|err| anyhow!("Could not set focus to canvas! {:#?}", err))
+            })
     } else {
         Ok(())
     }
