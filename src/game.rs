bbc1ae9341b9a6ee0833efd8baa40c2f5ebcdc6a
Takahito Nakano <kano@ponko2.jp>
2024-01-08 22:06:21 +0900

    Chapter 9.2.4

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,13 @@
+#[cfg(test)]
+mod test_browser;
+#[cfg(test)]
+use test_browser as browser;
+
+#[cfg(not(test))]
+use crate::browser;
+
 use self::red_hat_boy_states::*;
 use crate::{
-    browser,
     engine::{
         self, Audio, Cell, Game, Image, KeyState, Point, Rect, Renderer, Sheet, Sound, SpriteSheet,
     },
@@ -1066,4 +1073,54 @@ fn rightmost(obstacle_list: &[Box<dyn Obstacle>]) -> i16 {
         .map(|obstacle| obstacle.right())
         .max_by(|x, y| x.cmp(y))
         .unwrap_or(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use futures::channel::mpsc::unbounded;
+    use std::collections::HashMap;
+    use web_sys::{AudioBuffer, AudioBufferOptions};
+
+    #[test]
+    fn test_transition_from_game_over_to_new_game() {
+        let (_, receiver) = unbounded();
+        let image = HtmlImageElement::new().unwrap();
+        let audio = Audio::new().unwrap();
+        let options = AudioBufferOptions::new(1, 3000.0);
+        let sound = Sound {
+            buffer: AudioBuffer::new(&options).unwrap(),
+        };
+        let rhb = RedHatBoy::new(
+            Sheet {
+                frames: HashMap::new(),
+            },
+            image.clone(),
+            audio,
+            sound,
+        );
+        let sprite_sheet = SpriteSheet::new(
+            Sheet {
+                frames: HashMap::new(),
+            },
+            image.clone(),
+        );
+        let walk = Walk {
+            boy: rhb,
+            backgrounds: [
+                Image::new(image.clone(), Point { x: 0, y: 0 }),
+                Image::new(image.clone(), Point { x: 0, y: 0 }),
+            ],
+            obstacles: vec![],
+            obstacle_sheet: Rc::new(sprite_sheet),
+            stone: image.clone(),
+            timeline: 0,
+        };
+        let state = WalkTheDogState {
+            _state: GameOver {
+                new_game_event: receiver,
+            },
+            walk,
+        };
+    }
 }
